3a5b7c0a570ed0e3e422dd006c5526b003cb7986
Ashley Thomas <thomas.ashley.r@gmail.com>
2021-06-10 17:46:00 -0400

    pushing existing repo

---
+++ friendly-parakeet/Develop/script.js
@@ -0,0 +1,27 @@
+// Assignment code here
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+
+  var length = Number(prompt("How many characters would you like your password to be?"));
+  var uppers = confirm("Would you like to use uppercase letters?");
+  var lowers = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+  
+return(password)
+
+}
