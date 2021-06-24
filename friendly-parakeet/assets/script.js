fa79bdb73e159f38b062f298542a657897ec9985
Ashley Thomas <thomas.ashley.r@gmail.com>
2021-06-23 20:37:38 -0400

    grade

--- friendly-parakeet/assets/script.js
+++ friendly-parakeet/assets/script.js
@@ -2,6 +2,17 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var chosenChars = []
+var lowerCase = alphabet.split('')
+var upperCase = alphabet.toUpperCase().split('')
+var numbers = "0,1,2,3,4,5,6,7,8,9"
+var symbols = "`,~.!,=,+,/,?,.,>,',"
+console.log(lowerCase)
+console.log(upperCase)
+console.log(numbers)
+console.log(symbols)
 
 // Write password to the #password input
 function writePassword() {
@@ -14,14 +25,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function writePassword() {
+function generatePassword() {
 
   var length = Number(prompt("How many characters would you like your password to be?"));
   var uppers = confirm("Would you like to use uppercase letters?");
   var lowers = confirm("Would you like to use lowercase letters?");
   var numbers = confirm("Would you like to use numbers?");
   var symbols = confirm("Would you like to use special characters?");
-  
-return(password)
+
+
+  var newPassword = ''
+  if (uppers) {
+    newPassword = ""
+  }
+  if (lowers) {
+    //dosomething
+  }
+  // else{
+
+  // }
+  if (numbers) {
+    //dosomething
+  }
+  if (symbols) {
+    //dosomething
+  }
+
+
+  return (newPassword)
 
 }
